2bb3530452e9a99a266765b8813078e777845d26
luxxx <jahodaluk82@gmail.com>
2021-01-31 22:29:06 +0100

    vytvorenie komponentu hero pre index.php

---
+++ components/index-hero.php
@@ -0,0 +1,10 @@
+<div style="border-radius: 10px; -webkit-box-shadow: 11px 11px 14px -5px rgba(81,81,81,0.67); 
+            box-shadow: 11px 11px 14px -5px rgba(81,81,81,0.67);" class="jumbotron">
+                <h1 class="display-4">Hello, world!</h1>
+                <p class="lead">Toto je jednoduchá stránka na to aby som dostal nejakú známku XD</p>
+                <hr class="my-4">
+                <p>ENJOY</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Link do pekla</a>
+                </p>
+            </div>
