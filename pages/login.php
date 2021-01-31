976ea4ebab8dd465c5ae8ca5b9e3e4930ca6cdbe
luxxx <jahodaluk82@gmail.com>
2021-01-31 22:12:39 +0100

    zfukčnenie overovania prihlásnia

--- pages/login.php
+++ pages/login.php
@@ -23,44 +23,47 @@ $pdo = new pdo(
 $error = "";
 $error_pass = "";
 $error_pass2 = "";
+$menicko = "";
+$password = "";
+$hesielko = "";
 
-$sth = $pdo->prepare("SELECT * FROM people");
-$sth->execute();
-
-$row = $sth->fetch(PDO::FETCH_ASSOC);
-$menicko = $row['meno'];
-$password = $row['hesielko'];
 
 echo $password;
 if(isset($_POST['bimbambum'])){
+   
+    $meno = $_POST['name'];
+    $pass = $_POST['password'];
+    $sth = $pdo->prepare("SELECT * FROM people WHERE meno = ?");
+    $sth->execute(array($meno));
+    if($sth->rowCount() == 1){
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        $password = $row['hesielko'];
+        $menicko = $row['meno'];
+    }
+    if(empty($_POST['password'])){
+        $error_pass = "<div class='alert alert-danger' role='alert'>Nezadal si heslo!</div>";    
+    } else if(isset($_POST['password'])){
+        
+        if(password_verify($pass, $password)){
+            $error_pass = ""; 
+        } else {
+        $error_pass = "<div class='alert alert-danger' role='alert'>Zlé heslo!</div>";
+        } 
+    }
+    
     if(empty($_POST['name'])){
-        $error = "<div class='alert alert-danger' role='alert'>
-        Nezadal si meno!
-        </div>";
-    } else if($_POST['name'] !== $menicko){
+        $error = "<div class='alert alert-danger' role='alert'>Nezadal si meno!</div>";  
+    } else if($meno == $menicko){
+        $error = "";
+    } else {
         $error = "<div class='alert alert-danger' role='alert'>
         Neexistuje žiaden účet s týmto menom
         </div>";
-    } else {
-        $error = "";
-    }
-    if(empty($_POST['password'])){
-        $error_pass = "<div class='alert alert-danger' role='alert'>Nezadal si heslo!</div>";
-    } else if(isset($_POST['password'])){
-        $pass = $_POST['password'];
-        if(password_verify($pass, $password)){
-            $error_pass = ""; 
-        } 
-    }      
-    } else {
-        $error_pass = "<div class='alert alert-danger' role='alert'>Zlé heslo!</div>";
-    }
+    } 
     if($error == "" && $error_pass == ""){
         header("location:../index.php");
     }
-    
-    echo $password;
-    echo $pass;
+}
 
 
 ?>
@@ -80,12 +83,11 @@ if(isset($_POST['bimbambum'])){
             box-shadow: 11px 11px 14px -5px rgba(81,81,81,0.67); text-align: center;" class="jumbotron">
                 <div><?php echo $error ?></div>
                 <div><?php echo $error_pass ?></div>
-                <div><?php echo $error_pass2 ?></div>
                 <h2>Prihláste sa!</h2>
                 <div style="padding: 5% 25% 0% 25%">
                     <form method="post" action="">
                         <label>Meno</label><input type="text" name="name" class="form-control">
-                        <label>Heslo</label><input type="password" name="password" class="form-control">
+                        <label>Heslo</label><input type="password" name="password" class="form-control"><br>
                         <input type="submit" value="Prihlásiť sa" name="bimbambum" class="btn btn-success">
                     </form>
                 </div>
